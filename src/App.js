fc338afbe305d19a3a52ea02e66abcee1b872e91
Manish Jain <manish.j2@samsung.com>
2024-07-31 11:20:09 +0530

    testing pre commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import './App.css';
 import AppRouter from './Layout/AppRouter/AppRouter';
 import Header from './Layout/Header/Header';
 
-function App() {
+const App = () => {
   return (
     <div className="App container">
       <Header />
       <AppRouter />
     </div>
   );
-}
+};
 
 export default App;
